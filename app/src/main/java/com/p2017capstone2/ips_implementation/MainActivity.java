9e880bf8d6149295db73e46a6741c1835a29a42f
kuman514 <7iggif@naver.com>
2017-11-13 15:39:18 +0900

    init

---
+++ app/src/main/java/com/p2017capstone2/ips_implementation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.p2017capstone2.ips_implementation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
